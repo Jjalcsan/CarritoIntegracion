98cc198a1e1c2761564621c391d2d0711ce32039
Jjalcsan <jjalcsan@gmail.com>
2022-06-07 12:25:36 +0200

    commit

--- src/test/java/com/example/demo/CarritoJpaApplicationTests.java
+++ src/test/java/com/example/demo/CarritoJpaApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class CarritoJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue("This will succeed.", true);
 	}
 
 }
